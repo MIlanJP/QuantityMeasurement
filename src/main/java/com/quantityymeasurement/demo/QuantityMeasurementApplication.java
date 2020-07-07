39b77b852fffd292a0ded4ff1d828e9728a145d8
kyoolbaba <kyoolbaba@gmail.com>
2020-07-07 10:22:16 +0530

    Added swagger2 configuration and swager io

--- src/main/java/com/quantityymeasurement/demo/QuantityMeasurementApplication.java
+++ src/main/java/com/quantityymeasurement/demo/QuantityMeasurementApplication.java
@@ -2,12 +2,24 @@ package com.quantityymeasurement.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class QuantityMeasurementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuantityMeasurementApplication.class, args);
+    }
+
+
+    public Docket swaggerConfiguration(){
+        return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.ant("/api/*")).
+                apis(RequestHandlerSelectors.basePackage("com.quantityymeasurement.demo")).build();
     }
 
 }
